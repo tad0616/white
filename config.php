7edad180a95c93f863160ef7d009354f6532cdd3
Tad <tad0616@gmail.com>
2015-06-26 15:17:27 +0800

    調整版面

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $theme_kind = 'mix';
 $theme_kind_arr = 'mix';
 
 //引入哪些選單？ all(含 my_menu,admin,user),my_menu,admin,user
-$menu_var_kind = 'all';
+$menu_var_kind = 'my_menu';
 
 //額外顏色設定 如： bootstrap3/themes/light/Cerulean
 $theme_color = 'bootstrap3';
@@ -192,16 +192,16 @@ $config_tabs[6] = 1;
 $config_enable['navbar_pos'] = array('enable' => '0', 'min' => '', 'max' => '', 'require' => '0', 'default' => 'default');
 
 //導覽工具列 漸層顏色(top)[theme_css_navbar.html]
-$config_enable['navbar_bg_top'] = array('enable' => '0', 'min' => '', 'max' => '', 'require' => '0', 'default' => '#B2ADA4');
+$config_enable['navbar_bg_top'] = array('enable' => '0', 'min' => '', 'max' => '', 'require' => '0', 'default' => 'transparent');
 
 //導覽工具列 漸層顏色(bottom)[theme_css_navbar.html]
-$config_enable['navbar_bg_bottom'] = array('enable' => '0', 'min' => '', 'max' => '', 'require' => '0', 'default' => '#B2ADA4');
+$config_enable['navbar_bg_bottom'] = array('enable' => '0', 'min' => '', 'max' => '', 'require' => '0', 'default' => 'transparent');
 
 //導覽工具列 連結區塊底色[theme_css_navbar.html]
-$config_enable['navbar_hover'] = array('enable' => '0', 'min' => '', 'max' => '', 'require' => '0', 'default' => '#827E78');
+$config_enable['navbar_hover'] = array('enable' => '0', 'min' => '', 'max' => '', 'require' => '0', 'default' => 'transparent');
 
 //上傳導覽列背景圖[navbar.html]，值：可指定置於「themes/佈景/images/nav_bg/」下的某一檔案名稱
-$config_enable['navbar_img'] = array('enable' => '1', 'min' => '', 'max' => '', 'require' => '0', 'default' => 'menu_background.gif');
+$config_enable['navbar_img'] = array('enable' => '0', 'min' => '', 'max' => '', 'require' => '0', 'default' => '');
 
 //導覽工具列 文字顏色[theme_css_navbar.html]
 $config_enable['navbar_color'] = array('enable' => '1', 'min' => '', 'max' => '', 'require' => '0', 'default' => '#FFFFFF');
